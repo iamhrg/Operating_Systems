1a222da7ae29f1d2d0f587084a0af3c0c55ef3a3
Harsh Gandhi <hrgandhi1@gmail.com>
2019-01-23 19:22:35 +0530

    Create Producer_Consumer_Bounded.py

---
+++ Producer_Consumer_Bounded.py
@@ -0,0 +1,47 @@
+//This code is entitled to Harsh Gandhi.
+#Bounded Buffer Producer Consumer Problem
+
+from threading import Thread, Condition
+import time
+import random
+
+queue = []
+MAX_NUM = 10 #Buffer Size
+condition = Condition()
+
+class ProducerThread(Thread):
+    def run(self):
+        nums = range(5)
+        global queue
+        while True:
+            condition.acquire()
+            if len(queue) == MAX_NUM:
+                print "Queue full, producer is waiting"
+                condition.wait()
+                print "Space in queue, Consumer notified the producer"
+            num = random.choice(nums)
+            queue.append(num)
+            print "Produced", num
+            condition.notify()
+            condition.release()
+            time.sleep(random.random())
+
+
+class ConsumerThread(Thread):
+    def run(self):
+        global queue
+        while True:
+            condition.acquire()
+            if not queue:
+                print "Nothing in queue, consumer is waiting"
+                condition.wait()
+                print "Producer added something to queue and notified the consumer"
+            num = queue.pop(0)
+            print "Consumed", num
+            condition.notify()
+            condition.release()
+            time.sleep(random.random())
+
+
+ProducerThread().start()
+ConsumerThread().start()
